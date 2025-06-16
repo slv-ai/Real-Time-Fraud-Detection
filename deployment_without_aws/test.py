aeeca3a6e2877d56d2478de98a91d16adc38dfb6
Porselvi <112519460+slv-ai@users.noreply.github.com>
2025-06-16 20:01:44 +0000

    modified lambda handler

--- deployment_without_aws/test.py
+++
@@ -1,8 +0,0 @@
-import requests
-data = {
-
-}
-
-url='http://localhost:9696/predict'
-response=requests.post(url,json=data)
-print(response.json())
--- streaming/lambda_function.py
+++ streaming/lambda_function.py
@@ -4,32 +4,46 @@ import boto3
 import base64
 import mlflow
 import joblib
+from io import BytesIO
+import pandas as pd
+import numpy as np
+import logging
 
+#setup logging
+logger=logging.getLogger()
+logger.setLevel(logging.INFO)
+
+#boto3 clients
 kinesis_client=boto3.client('kinesis')
+s3 = boto3.client('s3')
 
+#environment variables
 PREDICTIONS_STREAM_NAME=os.getenv("PREDICTIONS_STREAM_NAME",'fraud_detections')
-
 RUN_ID=os.getenv("RUN_ID")
+MODEL_BUCKET = os.getenv("MODEL_BUCKET",'mlflow-fraud-detection-slv')
+TEST_RUN=os.getenv('TEST_RUN','False') == 'True'
 
+#load model
 logged_model=f's3://mlflow-fraud-detection-slv/1/{RUN_ID}/artifacts/model'
 model=mlflow.pyfunc.load_model(logged_model)
 
-TEST_RUN=os.getenv('TEST_RUN','False') == 'True'
+# load label encoders
+def load_label_encoders():
+    obj = s3.get_object(Bucket=MODEL_BUCKET, Key='models/label_encoders.pkl')
+    return joblib.load(BytesIO(obj['Body'].read()))
 
 
-def load_encoders():
-    global encoders 
-    s3=boto3.client('s3')
-    #load label encoders from s3
-    s3.download_file()
-
-def preprocess_features(df):
+def preprocess_features(df,label_encoders):
     cat_cols=df.select_dtypes(include=['object', 'category']).columns
     for col in cat_cols:
         df[col]=df[col].fillna('Unknown').astype(str)
-        #fit label encoder
-        le=LabelEncoder()
-        df[col]=le.fit_transform(df[col])
+        #use prefitted laebl encoder
+        le=label_encoders.get(col)
+        if le:
+            df[col]=le.transform(df[col])
+        else :
+            logger.warning(f"No label encoder found for column: {col}, defaulting to 0")
+            df[col]=0       
         
     return df
 
@@ -38,8 +52,8 @@ def predict(features):
     return pred[0]
 
 def lambda_handler(event,context):
-
-    predicions_events =[]
+    prediction_events =[]
+    
 
     for record in event['Records']:
         encoded_data=record['kinesis']['data']
@@ -47,14 +61,16 @@ def lambda_handler(event,context):
 
         record_data = json.loads(decoded_data)
         TransactionID = record_data['TransactionID']
-
+        
         #convert dict into dataframe
         features_df=pd.DataFrame([record_data])
 
         #replace null with np.nan
-        features_df=features_df.replace({None : np.nan})
-
-        features_df = preprocess_features(features_df)
+        features_df=features_df.where(pd.notnull(features_df), np.nan)
+        #load encoders
+        label_encoders = load_label_encoders()
+        #preprocess features & predict
+        features_df = preprocess_features(features_df,label_encoders)
         prediction=predict(features_df)
 
         prediction_event ={
@@ -68,12 +84,12 @@ def lambda_handler(event,context):
 
         if not TEST_RUN:
             kinesis_client.put_record(
-                Stream_name=PREDICTIONS_STREAM_NAME,
+                StreamName=PREDICTIONS_STREAM_NAME,
                 data = json.dumps(prediction_event),
                 partition_key = TransactionID
             )
 
-        predicions_events.append(predicion_event)
+        prediction_events.append(predicion_event)
 
     return{
         'predictions':predicions_events
